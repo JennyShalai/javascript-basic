a319cf21f3d3a3b49533471a487edc2cb94b4abf
Jenny Shalai <jenny.shalai@gmail.com>
2019-06-05 13:24:30 -0400

    add, strings and hashmap funtions

---
+++ data-structures.js
@@ -0,0 +1,44 @@
+// STRING
+// creat
+var string = '';
+
+// modify
+var string = string + valueOMGofAnyType;
+var string = str1.concat(str2);
+
+//char at specific position
+var char = string.charAt(3);
+
+//number of char in string
+var length = string.length
+
+//other build-in functions
+var string = 'Hello world!';
+var booleanCheck = string.endsWith('world!');
+var booleanCheck = string.includes('world!');
+
+//Find the first occurrence of the letter in a string
+var charOccurrence = str.indexOf(char)
+var charOccurrence = str.indexOf(char, position)
+
+//
+
+// ARRAY
+
+// HASH MAP
+// creat
+var hash = {};
+
+// add key-value
+hash[key] = value;
+
+// modify value
+hash[key] = [newValue];
+hash[key] += 1;
+
+// loop through
+for (var key in hash) {
+	console.log(hash[key]);
+}
+
+
