9af430fef37085f442d755e964fb960c52e8ba66
Jenny Shalai <jenny.shalai@gmail.com>
2019-06-05 14:05:12 -0400

    add string functions

--- data-structures.js
+++ data-structures.js
@@ -1,31 +1,43 @@
-// STRING
+//// STRING
 // creat
 var string = '';
 
 // modify
 var string = string + valueOMGofAnyType;
 var string = str1.concat(str2);
+// multiplying 
+var doubleString = string.repeat(2);
 
-//char at specific position
-var char = string.charAt(3);
-
-//number of char in string
+// number of char in string
 var length = string.length
 
-//other build-in functions
+// replace
+var string = string.replace('whatToReplace', 'newValue')
+
+// substring
+var subString = string.slice(fromIndex, toIndex);
+
+// char at specific position
+var char = string.charAt(3);
+// other build-in functions
 var string = 'Hello world!';
+var booleanCheck = string.startsWith('Hello');
 var booleanCheck = string.endsWith('world!');
 var booleanCheck = string.includes('world!');
-
-//Find the first occurrence of the letter in a string
+// find the first occurrence of the letter in a string
 var charOccurrence = str.indexOf(char)
 var charOccurrence = str.indexOf(char, position)
+// last coourrence of the letter or string
+var charLastOccurrence = str.lastIndexOf(char)
+// position of the match
+var number = string.search('value'); // -1 if not found
 
-//
+// array of substrings
+var array = string.split('valueOfSeparator')
 
-// ARRAY
+//// ARRAY
 
-// HASH MAP
+//// HASH MAP
 // creat
 var hash = {};
 
